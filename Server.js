2ed39803f4ce3bc45718593565a6b3b5b19dbe4a
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-09-29 02:11:55 -0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -10,32 +9,10 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Configuração do Nodemailer
-const transporter = nodemailer.createTransport({
-    service: 'gmail', // ou outro serviço de e-mail
-    auth: {
-        user: 'seuemail@gmail.com', // seu e-mail
-        pass: 'suasenha' // sua senha ou senha de aplicativo
-    }
-});
-
-// Rota para enviar e-mail
-app.post('/send-notification', (req, res) => {
-    const { email, debtName, dueDate } = req.body;
-
-    const mailOptions = {
-        from: 'seuemail@gmail.com',
-        to: email,
-        subject: 'Notificação de Vencimento de Dívida',
-        text: `A dívida "${debtName}" vence em ${dueDate}. Por favor, não se esqueça de pagá-la.`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.status(500).send(error.toString());
-        }
-        res.status(200).send('Notificação enviada: ' + info.response);
-    });
+// Rota para adicionar dívida (sem e-mail)
+app.post('/add-debt', (req, res) => {
+    // Lógica para adicionar dívida (ainda precisa ser implementada)
+    res.status(200).send('Dívida adicionada com sucesso');
 });
 
 // Inicia o servidor
