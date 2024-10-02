7e15de75fa921b4a53f0d03c00011d5408249036
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-02 06:51:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-// Import the functions you need from the SDKs you need
+// script.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
-// Initialize Firebase Authentication
-const auth = getAuth();
+const firebaseConfig = {
+    apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
+    authDomain: "dividas1-fed53.firebaseapp.com",
+    projectId: "dividas1-fed53",
+    storageBucket: "dividas1-fed53.appspot.com",
+    messagingSenderId: "350859669404",
+    appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
+    measurementId: "G-7HGSN6TC3Y"
+};
 
-// Adiciona um ouvinte para o formulário de login
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 document.getElementById("loginForm").addEventListener("submit", (event) => {
-    event.preventDefault(); // Evita o envio do formulário
+    event.preventDefault();
 
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPassword").value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            const user = userCredential.user;
-            console.log("Usuário autenticado:", user);
+            console.log("Usuário autenticado:", userCredential.user);
             alert("Login bem-sucedido!");
-            window.location.href = 'pagina-de-dividas.html'; // Redireciona após login
+            window.location.href = 'pagina-de-dividas.html';
         })
         .catch((error) => {
-            const errorMessage = error.message;
-            console.error("Erro ao fazer login:", errorMessage);
-            alert("Erro de login: " + errorMessage);
+            console.error("Erro ao fazer login:", error.message);
+            alert("Erro de login: " + error.message);
         });
 });
