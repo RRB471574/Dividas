4a865f10008b815ad4ac7271792537dcb99effb0
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-02 06:50:43 -0300

    Update register.js

--- register.js
+++ register.js
@@ -1,26 +1,37 @@
-// Import the functions you need from the SDKs you need
+// register.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
-// Initialize Firebase Authentication
-const auth = getAuth();
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
+    authDomain: "dividas1-fed53.firebaseapp.com",
+    projectId: "dividas1-fed53",
+    storageBucket: "dividas1-fed53.appspot.com",
+    messagingSenderId: "350859669404",
+    appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
+    measurementId: "G-7HGSN6TC3Y"
+};
 
-// Adiciona um ouvinte para o formulário de registro
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 document.getElementById("registerForm").addEventListener("submit", (event) => {
-    event.preventDefault(); // Evita o envio do formulário
+    event.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            const user = userCredential.user;
-            console.log("Usuário registrado:", user);
+            console.log("Usuário registrado:", userCredential.user);
             alert("Registro bem-sucedido!");
-            window.location.href = 'index.html'; // Redireciona para a página de login após o registro
+            window.location.href = 'index.html';
         })
         .catch((error) => {
-            const errorMessage = error.message;
-            console.error("Erro ao registrar:", errorMessage);
-            alert("Erro de registro: " + errorMessage);
+            console.error("Erro ao registrar:", error.message);
+            alert("Erro de registro: " + error.message);
         });
 });
