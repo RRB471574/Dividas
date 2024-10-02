a418768b34962e7893e745a2791ff91543bfb9d1
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-02 05:47:24 -0300

    Update register.js

--- register.js
+++ register.js
@@ -1,10 +1,8 @@
 // register.js
 
-// Importar funções do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 
-// Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
     authDomain: "dividas1-fed53.firebaseapp.com",
@@ -15,27 +13,22 @@ const firebaseConfig = {
     measurementId: "G-7HGSN6TC3Y"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Executa o código após o DOM estar completamente carregado
 document.addEventListener("DOMContentLoaded", () => {
     const registerForm = document.getElementById("registerForm");
-    const errorMessage = document.getElementById("error-message"); // Elemento para exibir mensagens de erro
+    const errorMessage = document.getElementById("error-message");
 
     registerForm.addEventListener("submit", (event) => {
-        event.preventDefault(); // Previne o envio padrão do formulário
+        event.preventDefault();
 
-        const email = registerForm.email.value;
-        const password = registerForm.password.value;
+        const email = registerForm.querySelector('input[name="email"]').value;
+        const password = registerForm.querySelector('input[name="password"]').value;
 
-        // Tenta criar um novo usuário
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                // Registro bem-sucedido
                 console.log("Usuário registrado:", userCredential.user);
-                // Redireciona para a página de dívidas ou para onde desejar
                 window.location.href = "pagina-de-dividas.html";
             })
             .catch((error) => {
@@ -43,8 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const errorMessageText = error.message;
 
                 console.error("Erro de registro:", errorMessageText);
-
-                // Verifica se o erro é de e-mail já em uso
+                
                 if (errorCode === "auth/email-already-in-use") {
                     errorMessage.textContent = "Este e-mail já está em uso. Por favor, tente outro.";
                 } else {
