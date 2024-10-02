229aa00a19b3fa36aeabed7589b008f35af4a982
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-02 16:25:59 -0300

    Update pagina-de-dividas.js

--- pagina-de-dividas.js
+++ pagina-de-dividas.js
@@ -1,3 +1,17 @@
+// Inicialize o Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
+    authDomain: "dividas1-fed53.firebaseapp.com",
+    projectId: "dividas1-fed53",
+    storageBucket: "dividas1-fed53.appspot.com",
+    messagingSenderId: "350859669404",
+    appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
+    measurementId: "G-7HGSN6TC3Y"
+};
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 
 auth.onAuthStateChanged((user) => {
