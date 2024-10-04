43fc1b7d79d0902e0d03da3096298d2e1dbe90d4
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-04 02:25:39 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,6 @@
-// Importa as funções necessárias do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-// Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
     authDomain: "dividas1-fed53.firebaseapp.com",
@@ -10,26 +8,30 @@ const firebaseConfig = {
     storageBucket: "dividas1-fed53.appspot.com",
     messagingSenderId: "350859669404",
     appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
+    measurementId: "G-7HGSN6TC3Y"
 };
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-document.getElementById('login-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+const loginForm = document.getElementById("login-form");
+const loginErrorMessage = document.getElementById("login-error-message");
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            const user = userCredential.user;
-            console.log('Usuário logado:', user);
-            window.location.href = 'pagina-de-dividas.html'; // Redireciona para a página de dívidas
+            console.log("Usuário logado:", userCredential.user);
         })
         .catch((error) => {
-            const errorMessage = document.getElementById('error-message');
-            errorMessage.textContent = error.message; // Exibe a mensagem de erro
-            console.error('Erro ao fazer login:', error);
+            const errorCode = error.code;
+            const errorMessageText = error.message;
+
+            loginErrorMessage.textContent = "Erro ao fazer login: " + errorMessageText;
         });
 });
