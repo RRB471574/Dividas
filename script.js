15bbf30327e30c2a07c9469c121f3e9827497b8a
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-04 02:03:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Importa as funções necessárias do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
