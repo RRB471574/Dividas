d0da487198252f865247cd50977b1e0cb84635c2
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-08 22:38:38 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,11 @@
 // Configurações do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
+  apiKey: "API_KEY_AQUI",
   authDomain: "dividas1-fed53.firebaseapp.com",
   projectId: "dividas1-fed53",
   storageBucket: "dividas1-fed53.appspot.com",
   messagingSenderId: "350859669404",
-  appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
-  measurementId: "G-YOURMEASUREMENTID"
+  appId: "1:350859669404:web:9b9ba5f6320ec92923a259"
 };
 
 // Inicializar Firebase
