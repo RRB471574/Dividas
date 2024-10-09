b89dad086b648f7591b4f27c9c2a56eb9e29fd17
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-08 21:58:00 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,28 +1,29 @@
-// Inicializar o Firebase
+// Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
-    authDomain: "dividas1-fed53.firebaseapp.com",
-    projectId: "dividas1-fed53",
-    storageBucket: "dividas1-fed53.appspot.com",
-    messagingSenderId: "350859669404",
-    appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
-    measurementId: "G-7HGSN6TC3Y"
+  apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
+  authDomain: "dividas1-fed53.firebaseapp.com",
+  projectId: "dividas1-fed53",
+  storageBucket: "dividas1-fed53.appspot.com",
+  messagingSenderId: "350859669404",
+  appId: "1:350859669404:web:9b9ba5f6320ec92923a259"
 };
+
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
 
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            console.log("Login bem-sucedido");
-            window.location.href = "pagina-de-dividas.html";
+    auth.signInWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            // Login bem-sucedido
+            window.location.href = 'pagina-de-dividas.html';
         })
-        .catch((error) => {
-            console.error("Erro ao fazer login:", error.message);
-            alert("Erro ao fazer login: " + error.message);
+        .catch(error => {
+            document.getElementById('error-message').innerText = error.message;
         });
 });
