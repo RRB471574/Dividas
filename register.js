7919071470ae1d1abe0532fae6213ca8260a06fe
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-08 22:32:31 -0300

    Update register.js

--- register.js
+++ register.js
@@ -1,30 +1,30 @@
-// Configurações do Firebase (substitua pelos valores da sua API Firebase)
+// Configurações do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
   authDomain: "dividas1-fed53.firebaseapp.com",
   projectId: "dividas1-fed53",
   storageBucket: "dividas1-fed53.appspot.com",
   messagingSenderId: "350859669404",
-  appId: "1:350859669404:web:9b9ba5f6320ec92923a259"
+  appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
+  measurementId: "G-YOURMEASUREMENTID"
 };
 
-// Inicializando Firebase
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 
-// Formulário de Registro
-document.getElementById('registerForm').addEventListener('submit', function(event) {
+document.getElementById("registerForm").addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
     auth.createUserWithEmailAndPassword(email, password)
-        .then(userCredential => {
-            // Registro bem-sucedido
-            window.location.href = 'login.html';
+        .then((userCredential) => {
+            window.location.href = "pagina-de-dividas.html";
         })
-        .catch(error => {
-            document.getElementById('error-message').innerText = error.message;
+        .catch((error) => {
+            document.getElementById("error-message").innerText = error.message;
         });
 });
