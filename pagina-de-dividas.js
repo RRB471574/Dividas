114db09aad7bd0b0fddc3eab46796739328f0e49
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2024-10-08 21:59:14 -0300

    Update pagina-de-dividas.js

--- pagina-de-dividas.js
+++ pagina-de-dividas.js
@@ -1,13 +1,19 @@
-// pagina-de-dividas.js
-
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js';
-import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js';
-
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
-    authDomain: "dividas1-fed53.firebaseapp.com",
-    projectId: "dividas1-fed53",
-    storageBucket: "dividas1-fed53.appspot.com",
-    messagingSenderId: "350859669404",
-    appId: "1:350859669404:web:9b9ba5f6320ec92923a259",
+  apiKey: "AIzaSyC3TUyXwtc9mD5463fEJd82BLGik9hwHrk",
+  authDomain: "dividas1-fed53.firebaseapp.com",
+  projectId: "dividas1-fed53",
+  storageBucket: "dividas1-fed53.appspot.com",
+  messagingSenderId: "350859669404",
+  appId: "1:350859669404:web:9b9ba5f6320ec92923a259"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+document.getElementById('logout').addEventListener('click', function() {
+    auth.signOut().then(() => {
+        window.location.href = 'login.html';
+    });
+});
