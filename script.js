accad39a9b958828d6230717d038223baedca033
RRB471574 <64625213+RRB471574@users.noreply.github.com>
2025-11-07 00:50:50 -0300

    Add welcome alert on page load

---
+++ script.js
@@ -0,0 +1,8 @@
+// Este é o motor do seu site.
+// Ele espera um pouquinho, e quando a página está pronta, faz o alerta.
+
+// A função 'window.onload' significa: "Quando a página terminar de carregar, faça isso:"
+window.onload = function() {
+    // A função 'alert()' abre uma janelinha na tela com a sua mensagem.
+    alert("BORA! Olá e seja muito bem-vindo ao nosso site!");
+};
